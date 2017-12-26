d568aa58a388748d6627c0cfa8cdd016a66a9f3d
weiyilee17 <weiyilee17@gmail.com>
2017-12-26 13:11:48 -0800

    had webpack work

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
+
+
+
+
+/*
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$, loader: 'babel-loader', exclude: /node_modules }
+    ]
+  }
+}
+*/
